bdf6032132947783df2e9963f514ac1d1a2c7452
sushiSushanshi <sushisushanshi13@gmail.com>
2025-10-31 00:07:46 +0530

    two

--- src/main/java/git/practice/demo/controller/Controller1.java
+++ src/main/java/git/practice/demo/controller/Controller1.java
@@ -23,6 +23,7 @@ public class Controller1 {
 		System.out.println("added one more log change in test 3 ");
 		System.out.println("added one more change in test 3 ");
 		System.out.println("added one more change in test 3 ");
+		System.out.println("two");
 		return "from cotroller 1 and test 2 and 3";
 	}
 }
